28eccd3a72459417a77f340f271e18bc2a6ece05
dartdaven <139046041+dartdaven@users.noreply.github.com>
2024-02-27 19:58:58 +0400

    Outlined what to do in Tree

--- TreasureMap/TreasureTree.h
+++ TreasureMap/TreasureTree.h
@@ -1,1 +1,38 @@
 #pragma once
+
+template<typename KeyType, typename ValueType>
+class TreasureTree
+{
+    struct Node
+    {
+        KeyType key;
+        ValueType value;
+        std::unique_ptr<Node> left;
+        std::unique_ptr<Node> right;
+
+        Node(const Key& k, const Value& v) : key(k), value(v) {}
+    }
+
+    std::unique_ptr<Node> root;
+
+public:
+    void insert(const KeyType& key, const ValueType& value)
+    {
+
+    }
+
+    void insert(const std::pair<KeyType, ValueType>& pair)
+    {
+        insert(pair.first, pair.second);
+    }
+
+    void erase(const KeyType& key)
+    {
+
+    }
+
+    ValueType& operator[](const KeyType& key)
+    {
+
+    }
+};
