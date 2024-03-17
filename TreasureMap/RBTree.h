92bc32ff10a95d2c12bbf9ab2bd4bf5c2e455280
dartdaven <139046041+dartdaven@users.noreply.github.com>
2024-03-17 19:42:11 +0400

    Trasitioned to shared_ptr's, rotations implemented
    - https://www.codesdope.com/course/data-structures-red-black-trees-insertion/

--- TreasureMap/RBTree.h
+++ TreasureMap/RBTree.h
@@ -13,15 +13,14 @@ namespace dvd
         {
             KeyType key;
             ValueType value;
-            std::unique_ptr<Node> left;
-            std::unique_ptr<Node> right;
+            std::shared_ptr<Node> left;
+            std::shared_ptr<Node> right;
             Color color;
             
-            //Should be shared pointer?
-            Node* parent;
+            std::weak_ptr<Node> parent;
 
-            //TODO define default color here
-            Node(const KeyType& k, const ValueType& v) : key(k), value(v), parent(nullptr) {}
+            Node(const KeyType& k, const ValueType& v, Color c = Color::Red, std::weak_ptr<Node> p = std::weak_ptr<Node>()) 
+                : key(k), value(v), color(c), parent(p) {}
 
             //Debug
             ~Node()
@@ -30,9 +29,61 @@ namespace dvd
             }
         };
 
-        std::unique_ptr<Node> m_Root;
+        std::shared_ptr<Node> m_Root;
 
         size_t m_Size;
+
+        //Utilities
+
+        std::shared_ptr<Node> find(const KeyType& key) const
+        {
+            std::shared_ptr<Node> iterator = m_Root;
+
+            while (iterator)
+            {
+                if (key == iterator->key) return iterator;
+                else if (key < iterator->key) iterator = iterator->left;
+                else iterator = iterator->right;
+            }
+
+            return nullptr;
+        }
+
+        void leftRotate(std::shared_ptr<Node>& x)
+        {
+            std::shared_ptr<Node> y = x->right;
+
+            x->right = y->left;
+
+            if (y->left != nullptr) y->left->parent = x;
+
+            y->parent = x->parent;
+
+            if (x->parent.lock() == nullptr) m_Root = y;
+            else if (x == x->parent.lock()->left) x->parent.lock()->left = y;
+            else x->parent.lock()->right = y;
+
+            y->left = x;
+            x->parent = y;
+        }
+
+        void rightRotate(std::shared_ptr<Node>& y)
+        {
+            std::shared_ptr<Node> x = y->left;
+
+            y->left = x->right;
+
+            if (x->right != nullptr) x->right->parent = y;
+
+            x->parent = y->parent;
+            
+            if (y->parent.lock() == nullptr) m_Root = x;
+            else if (y == y->parent.lock()->right) y->parent.lock()->right = x;
+            else y->parent.lock()->left = x;
+            
+            x->right = y;
+            y->parent = x;
+        }
 
     public:
         RBTree(std::initializer_list<std::pair<KeyType, ValueType>> initList)
@@ -46,37 +97,55 @@ namespace dvd
         //Without recursion
         void insert(const KeyType& key, const ValueType& value)
         {
-            std::unique_ptr<Node> newNode = std::make_unique<Node>(key, value);
-
             if (!m_Root)
             {
-                m_Root = std::move(newNode);
-                m_Root->color = Color::Black;
+                m_Root = std::make_shared<Node>(key, value, Color::Black);
                 ++m_Size;
 
                 return;
             }
 
-            Node* nodeToAttachTo = nullptr;
-            Node* iterator = m_Root.get();
+            std::shared_ptr<Node> nodeToAttachTo;
+            std::shared_ptr<Node> iterator = m_Root;
 
             //Searching for suitable place
             while (iterator)
             {
                 nodeToAttachTo = iterator;
                 
-                if (key < iterator->key) iterator = iterator->left.get();
-                else if (key > iterator->key) iterator = iterator->right.get();
+                if (key < iterator->key) iterator = iterator->left;
+                else if (key > iterator->key) iterator = iterator->right;
                 else
                 {
-                    iterator->value = value; //Update the value in node
+                    debug::Log("Node with this key already exists");
                     return;
                 }
             }
 
-            if (key < nodeToAttachTo->key) nodeToAttachTo->left = std::move(newNode);
-            else nodeToAttachTo->right = std::move(newNode);
+            if (key < nodeToAttachTo->key)
+            {
+                nodeToAttachTo->left = std::make_shared<Node>(key, value, Color::Red, nodeToAttachTo);
+            }
+            else nodeToAttachTo->right = std::make_shared<Node>(key, value, Color::Red, nodeToAttachTo);
+            
+            ++m_Size;
         }
+        
+        //debug
+        void leftRotate(const KeyType& key)
+        {
+            std::shared_ptr<Node> nodeToRotate = find(key);
+            if (nodeToRotate)
+                leftRotate(nodeToRotate);
+        }
+
+        void rightRotate(const KeyType& key)
+        {
+            std::shared_ptr<Node> nodeToRotate = find(key);
+            if (nodeToRotate)
+                rightRotate(nodeToRotate);
+        }
+
 
     };
 }
--- TreasureMap/main.cpp
+++ TreasureMap/main.cpp
@@ -6,23 +6,39 @@
 
 //TODO create a struct with debug messages on copy and move constructors and get through the map
 
+void test1()
+{
+    dvd::RBTree<int, std::string> numbers{ { 5, "Five" }, { 2, "Two" } };
+
+    numbers.insert(10, "Ten");
+    numbers.insert(8, "Eight");
+    numbers.insert(12, "Twelve");
+    numbers.insert(6, "Six");
+    numbers.insert(9, "Nine");
+
+    numbers.leftRotate(5);
+}
+
+void test2()
+{
+    dvd::RBTree<int, std::string> numbers{ {10, "Ten"}, { 15, "Fifteen" } };
+
+    numbers.insert(5, "Five");
+    numbers.insert(2, "Two");
+    numbers.insert(7, "Seven");
+    numbers.insert(6, "Six");
+    numbers.insert(8, "Eight");
+
+    numbers.leftRotate(5);
+
+    numbers.rightRotate(7);
+
+    std::cin.get();
+}
+
 int main()
 {
-    dvd::RBTree<int, std::string> numbers{ {8, "Eight"} , {9, "Nine"} };
-
-    numbers.insert(3, "Three");
-    numbers.insert(2, "Two");
-    numbers.insert(5, "Five");
-    numbers.insert(4, "Four");
-    numbers.insert(6, "Six");
-    //numbers.emplace(7, "Seven");
-
-    //if (numbers.contains(4)) debug::Log("Hello");
-
-    //numbers.erase(3);
-    //numbers.erase(4);
-
-    //if (!numbers.contains(4)) debug::Log("Bye");
+    test2();
 
     std::cin.get();
 }
